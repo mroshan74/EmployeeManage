5a1750ddd40fdc3b5eab4cbca2f9e48854f8f5d4
Muhammed Roshan <mroshan74@gmail.com>
2023-03-14 23:10:00 +0530

    repository pattern

---
+++ src/EmployeeManage.Infrastructure/GenericRepository.cs
@@ -0,0 +1,89 @@
+﻿using System.Linq.Expressions;
+using EmployeeManage.Common.Interfaces;
+using EmployeeManage.Common.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManage.Infrastructure;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+{
+    private readonly ApplicationDbContext _dbContext;
+    private DbSet<T> DbSet { get; }
+
+    public GenericRepository(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+        DbSet = dbContext.Set<T>();
+    }
+    
+    public async Task<List<T>> GetFilteredAsync(Expression<Func<T, bool>>[] filters, int? skip, int? take, params Expression<Func<T, object>>[] includes)
+    {
+        IQueryable<T> query = DbSet;
+        
+        foreach (var filter in filters)
+            query = query.Where(filter);
+        
+        foreach (var include in includes)
+            query = query.Include(include);
+
+        if (skip != null)
+            query = query.Skip(skip.Value);
+
+        if (take != null)
+            query = query.Take(take.Value);
+
+        return await query.ToListAsync();
+    }
+
+    public async Task<List<T>> GetAsync(int? skip, int? take, params Expression<Func<T, object>>[] includes)
+    {
+        IQueryable<T> query = DbSet;
+
+        foreach (var include in includes)
+            query = query.Include(include);
+
+        if (skip != null)
+            query = query.Skip(skip.Value);
+
+        if (take != null)
+            query = query.Take(take.Value);
+
+        return await query.ToListAsync();
+    }
+
+    public async Task<T?> GetByIdAsync(int id, params Expression<Func<T, object>>[] includes)
+    {
+        IQueryable<T> query = DbSet;
+
+        query = query.Where(entity => entity.Id == id);
+
+        foreach (var include in includes)
+            query = query.Include(include);
+
+        return await query.SingleOrDefaultAsync();
+    }
+
+    public async Task<int> InsertAsync(T entity)
+    {
+        await DbSet.AddAsync(entity);
+        return entity.Id;
+    }
+
+    public void Update(T entity)
+    {
+        DbSet.Attach(entity);
+        _dbContext.Entry(entity).State = EntityState.Modified;
+    }
+
+    public void Delete(T entity)
+    {
+        if (_dbContext.Entry(entity).State == EntityState.Detached)
+            DbSet.Attach(entity);
+        DbSet.Remove(entity);
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _dbContext.SaveChangesAsync();
+    }
+}
